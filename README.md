697effd8bd3dda675d9b07803d60e852c4e73299
Thiago <thiagobtw437@gmail.com>
2023-05-24 00:07:21 -0300

    Started transition to NextJS (fourth time is the charm? 😒)

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-page
-My personal portfolio page
--- resources/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(this).nextUntil("tr.clickable").slideToggle(100, function(){})
-
-    $("#projects > div > div > div.table-container > table > tbody > .clickable").click(function() {
-        $(this).nextUntil("tr.clickable").slideToggle(100, function(){})
-    });
-})
