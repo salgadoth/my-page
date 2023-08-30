c67e4df39afa77ad49d19129f4cc052bb5d00e11
Thiago <thiagobtw437@gmail.com>
2023-08-30 20:49:05 -0300

    Altered data display, for rendering the return from API

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -4,45 +4,16 @@ import CardContainer from '@/components/templates/CardContainer'
 import Cards from '@/components/templates/Cards'
 import Pagina from '@/components/templates/Pagina'
 import TopBar from '@/components/templates/TopBar'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function ExperiencePage(props: any) {
-  const data = [
-    {
-      _id: 'asuh1244uh1u1o1i2n32a',
-      title: 'Software developer',
-      company: 'VerdeCard',
-      lvl: 'Intern',
-      started: 'Sep 2020',
-      ended: 'March 2021',
-      cuntry: 'Brazil',
-      desc: 'Maintenance of Java SE and Spring Framework systems. Participation in small-sized projects.',
-    },
-    {
-      _id: 'aioghh139u2huhnfds',
-      title: 'Java Analyst Programmer',
-      company: 'VerdeCard',
-      lvl: 'Junior',
-      started: 'March 2021',
-      ended: 'March 2022',
-      cuntry: 'Brazil',
-      desc: 'Development of Java SE and Spring Framework systems. Participation in small and medium-sized projects.',
-    },
-    {
-      _id: 'sdihgbu2h4guyihbsqyg',
-      title: 'Java Analyst Programmer',
-      company: 'VerdeCard',
-      lvl: 'Mid',
-      started: 'March 2023',
-      ended: '',
-      cuntry: 'Brazil',
-      desc: 'Responsible for the development and maintenance of Java EE and Spring Framework systems. Participation in large-scale projects and multidisciplinary teams',
-    },
-  ]
+  const [responseData, setResponseData] = useState()
 
   useEffect(() => {
     async function getData() {
-      await fetch('http://localhost:3001/api/user')
+      const response = await fetch('http://localhost:3001/api/user')
+      const json = await response.json()
+      setResponseData(json.UserExperience)
     }
     getData()
   }, [])
@@ -56,7 +27,7 @@ export default function ExperiencePage(props: any) {
         classNameHeader="!text-white"
       >
         <CardContainer>
-          <Cards data={data} />
+          <Cards data={responseData} />
         </CardContainer>
       </Pagina>
     </>
