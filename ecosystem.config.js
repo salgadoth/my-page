adc20aeeb831488f89d314f25838a2f2d48ad345
Thiago Salgado <thiagobtw437@gmail.com>
2024-07-29 12:25:15 +1000

    Added actions-runner workflow

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'my-page',
+      script: 'npm',
+      args: 'start',
+      env: {
+        API_URL: process.env.API_URL,
+        NEXT_PUBLIC_VPS_URL: process.env.NEXT_PUBLIC_VPS_URL,
+      },
+    },
+  ],
+}
