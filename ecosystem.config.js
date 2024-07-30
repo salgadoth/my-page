2e31583d5028a50cd07f68322add08244f695a8a
Thiago Salgado <thiagobtw437@gmail.com>
2024-07-30 11:21:55 +1000

    Made some fixes in the action deployment file, altered startup listen port for the nextjs server.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'my-page',
       script: 'npm',
-      args: 'start',
+      args: 'run start',
       env: {
         API_URL: process.env.API_URL,
         NEXT_PUBLIC_VPS_URL: process.env.NEXT_PUBLIC_VPS_URL,
