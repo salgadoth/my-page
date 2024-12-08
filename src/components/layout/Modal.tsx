70a1c62933d15515fa219ca96d9fd73301262fab
Thiago Salgado <thiagobtw437@gmail.com>
2024-12-08 22:07:17 +1100

    Small details.

--- src/components/layout/Modal.tsx
+++ src/components/layout/Modal.tsx
@@ -38,10 +38,10 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
       {/* Modal Content */}
       <div
-        className={`w-[50%] h-[50%] fixed top-[30%] left-[25%] inset-0 flex items-center justify-center z-50 transform transition-transform rounded-lg bg-white shadow-lg p-6 ${animationClass}`}
+        className={`w-[50%] h-[50%] fixed top-[30%] left-[25%] inset-0 flex items-center justify-center z-50 transform transition-transform rounded-2xl bg-white shadow-lg p-6 ${animationClass}`}
       >
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className="absolute top-2 right-4 text-gray-500 hover:text-gray-700"
           onClick={onClose}
         >
           ✖
