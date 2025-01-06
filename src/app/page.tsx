1472af9c9bef5dec411e14eff5dea051ecdb1071
Thiago Salgado <thiagobtw437@gmail.com>
2025-01-07 01:32:29 +1100

    Implemented fetch from API to retrieve current position.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,37 @@
 import Apresentacao from '@/components/templates/Apresentacao'
 import Pagina from '@/components/templates/Pagina'
 import TopBar from '@/components/templates/TopBar'
+import { ExperienceModel } from '@/data/models/ExperienceModel'
+import { UserModel } from '@/data/models/UserModel'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
   // TODO: RETRIEVE CURRENT POSITION FROM API AND DISPLAY IT AS: "Currently I'm focused in building solutions for {current_position}."
+  const [userData, setUserData] = useState<ExperienceModel>()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('api/user')
+      .then((res) => res.json())
+      .then((data: UserModel) => {
+        console.log(data)
+        setLoading(false)
+        setUserData(data.curr_company)
+      })
+      .catch((err) => {
+        setLoading(false)
+        console.log(err)
+      })
+  }, [])
   return (
     <>
       <TopBar />
       <Pagina classNameParent="!bg-indigo" classNameHeader="pb-10">
-        <Apresentacao />
+        {!loading && userData ? (
+          <Apresentacao data={userData} />
+        ) : (
+          <Apresentacao />
+        )}
       </Pagina>
     </>
   )
--- src/components/templates/Apresentacao.tsx
+++ src/components/templates/Apresentacao.tsx
@@ -1,16 +1,23 @@
-export default function Apresentacao() {
+import { ExperienceModel } from '@/data/models/ExperienceModel'
+
+interface ApresentacaoProps {
+  data?: ExperienceModel
+}
+
+export default function Apresentacao({ data }: ApresentacaoProps) {
   return (
-    <div
-      className={`
-            font-kanit text-2xl
-        `}
-    >
+    <div className="font-kanit text-2xl">
       <p className="opacity-70">Hi, my name is</p>
       <p className="text-7xl font-kanitMed !opacity-100 py-3">Thiago Salgado</p>
       <p className="opacity-70 py-3">I build things for the web.</p>
       <p className="opacity-70">
         I am a FullStack software developer, specializing in building (and
-        occasionally leading) exceptional web experiences.
+        occasionally leading) exceptional web experiences.{' '}
+        {data?.company && (
+          <>
+            Currently I&apos;m focused in building solutions for {data.company}.
+          </>
+        )}
       </p>
     </div>
   )
