779ad74e9011c017aaa4fdd767413aa8baed44b2
Thiago Salgado <thiagobtw437@gmail.com>
2025-05-01 21:13:15 +1000

    Still trying to make deployment flow work.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd /var/www/my-page
+echo "Retrieving secrets..."
+aws secretsmanager get-secret-value \
+  --secret-id my-app-env \
+  --query SecretString \
+  --output text > .env
 echo "Installing dependencies..."
-sudo npm install --production
+sudo npm install --omit=dev
 echo "Building my-page app..."
 sudo npm run build
