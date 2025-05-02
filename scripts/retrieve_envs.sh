d2118660d8443eae763358c7f62a2a3be19687fa
Thiago Salgado <thiagobtw437@gmail.com>
2025-05-02 15:23:12 +1000

    Updated retrieve_envs script to fix error with permissions.

--- scripts/retrieve_envs.sh
+++ scripts/retrieve_envs.sh
@@ -1,6 +1,6 @@
 > ".env" # Clear the output file
 
-aws ssm get-parameters-by-path \
+sudo aws ssm get-parameters-by-path \
   --path "/mypage/env" \
   --with-decryption \
   --query "Parameters[*].{Name:Name,Value:Value}" \
@@ -13,5 +13,5 @@ aws ssm get-parameters-by-path \
     echo "$key=\"$value\"" >> ".env"
   done
 
-chmod 600 ".env" # Set permissions to read/write for the owner only
-chown $USER:$USER ".env" # Change ownership to the current user
+sudo chmod 600 ".env" # Set permissions to read/write for the owner only
+sudo chown $USER:$USER ".env" # Change ownership to the current user
