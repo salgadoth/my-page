f2c0dc11db97fd27df09eb25bfd2a2668c2ddb7a
Thiago Salgado <thiagobtw437@gmail.com>
2025-05-02 15:19:59 +1000

    Forget it all, the script name was misspelled in the appspec file.]

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-# Check if the PM2 process is running before stopping
-# This prevents errors if the process is not found
-# and allows the script to continue
-# without interruption.
 echo "Stopping existing my-page app (if running)..."
-if pm2 list | grep -q "my-page"; then
-  pm2 stop my-page
-else
-  echo "PM2 process 'my-page' not found, skipping stop."
-fi
+pm2 stop my-page || true
 echo "Removing old app files..."
 sudo rm -rf /var/www/my-page/*
