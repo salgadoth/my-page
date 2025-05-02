43490c3142e6a26939e73cf1f9a3b7915807e3a6
Thiago Salgado <thiagobtw437@gmail.com>
2025-05-02 15:22:12 +1000

    Updated retrieve_envs script to fix error with variables.

--- scripts/retrieve_envs.sh
+++ scripts/retrieve_envs.sh
@@ -1,21 +1,17 @@
-SSM_PATH = "/mypage/env"
-OUTPUT_FILE = ".env"
-AWS_REGION = "ap-southeast-2"
-
-> "$OUTPUT_FILE" # Clear the output file
+> ".env" # Clear the output file
 
 aws ssm get-parameters-by-path \
-  --path "$SSM_PATH" \
+  --path "/mypage/env" \
   --with-decryption \
   --query "Parameters[*].{Name:Name,Value:Value}" \
-  --region your-region \
+  --region ap-southeast-2 \
   --output text |
   while read name value; do
     # Extract the key without the path prefix
     key=$(basename "$name")
     # Extract the value into output file
-    echo "$key=\"$value\"" >> "$OUTPUT_FILE"
+    echo "$key=\"$value\"" >> ".env"
   done
 
-chmod 600 "$OUTPUT_FILE" # Set permissions to read/write for the owner only
-chown $USER:$USER "$OUTPUT_FILE" # Change ownership to the current user
+chmod 600 ".env" # Set permissions to read/write for the owner only
+chown $USER:$USER ".env" # Change ownership to the current user
