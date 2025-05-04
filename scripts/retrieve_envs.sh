40006022c1c2f83d173bf77ba6f9137120598d5c
Thiago Salgado <thiagobtw437@gmail.com>
2025-05-04 16:43:52 +1000

    Finally figured out ssm, cannot exec aws cli command with sudo, it will assume a different profile from the one defined in the credetials file, failing to comply with the IAM access policy.

--- scripts/retrieve_envs.sh
+++ scripts/retrieve_envs.sh
@@ -1,17 +1,17 @@
-> ".env" # Clear the output file
+> "../.env" # Clear the output file
 
-sudo aws ssm get-parameters-by-path \
+aws ssm get-parameters-by-path \
   --path "/mypage/env" \
   --with-decryption \
   --query "Parameters[*].{Name:Name,Value:Value}" \
   --region ap-southeast-2 \
-  --output text |
+  --output text | 
   while read name value; do
     # Extract the key without the path prefix
     key=$(basename "$name")
     # Extract the value into output file
-    echo "$key=\"$value\"" >> ".env"
+    echo "$key=\"$value\"" >> "../.env"
   done
 
-sudo chmod 600 ".env" # Set permissions to read/write for the owner only
-sudo chown $USER:$USER ".env" # Change ownership to the current user
+sudo chmod 600 "../.env" # Set permissions to read/write for the owner only
+sudo chown $USER:$USER "../.env" # Change ownership to the current user
