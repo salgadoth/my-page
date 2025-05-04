3b730ef09b0cb28e1fb81bc643fa864753666fda
Thiago Salgado <thiagobtw437@gmail.com>
2025-05-04 17:03:48 +1000

    Fixed start server script, it now utilizes prevously defined ecosystem file.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/my-page
 echo "Starting Next.js app with PM2..."
-pm2 start npm --name "my-page" -- --production
+pm2 start ecosystem.config.js --env production
 echo "Saving PM2 process list..."
 pm2 save
