7388cd32a74077eea2e897bb3589570fe8376a38
Thiago Salgado <thiagobtw437@gmail.com>
2025-06-06 19:14:00 +1000

    Changed dir of creation of the .env file, should create it on the root dir now.

--- scripts/retrieve_envs.sh
+++ scripts/retrieve_envs.sh
@@ -1,7 +1,7 @@
 # Create and set permissions for the .env file
-sudo touch "../.env" # Create the .env file if it doesn't exist
-sudo chmod 600 "../.env" # Set permissions to read/write for the owner only
-sudo chown $USER:$USER "../.env" # Change ownership to the current user
+sudo touch ".env" # Create the .env file if it doesn't exist
+sudo chmod 600 ".env" # Set permissions to read/write for the owner only
+sudo chown $USER:$USER ".env" # Change ownership to the current user
 
 # Retrieve secrets from AWS SSM Parameter Store
 # and write them to the .env file
@@ -15,5 +15,5 @@ aws ssm get-parameters-by-path \
     # Extract the key without the path prefix
     key=$(basename "$name")
     # Extract the value into output file
-    echo "$key=\"$value\"" >> "../.env"
+    echo "$key=\"$value\"" >> ".env"
   done
